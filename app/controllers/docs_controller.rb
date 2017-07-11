b4b3aa2b0538013796dbb0fdd8afab1a050c54c5
Julio Alvarez Pimiento <jalvarezpimiento@gmail.com>
2017-07-11 18:52:53 -0500

    Edit and Destroy options added

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,6 +1,6 @@
 class DocsController < ApplicationController
     #Will do this action before everything
-    before_action :find_doc, only: [:show, :edit, :destroy]
+    before_action :find_doc, only: [:show, :edit, :destroy, :update]
 
     def index
       @docs = Doc.all.order("created_at DESC")
@@ -27,9 +27,16 @@ class DocsController < ApplicationController
     end
 
     def update
+      if @doc.update(doc_params)
+        redirect_to @doc
+      else
+        render 'edit'
+      end
     end
 
     def destroy
+      @doc.destroy
+      redirect_to docs_path
     end
 
     private
