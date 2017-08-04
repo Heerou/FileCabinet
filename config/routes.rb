b056f95ceb372acdc60e83dfcd3a1393b3cb7b0e
Julio Alvarez Pimiento <jalvarezpimiento@gmail.com>
2017-08-04 07:00:22 -0500

    Change root index for an authenticated user

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,12 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #sets my root page to welcome/index
-  root 'welcome#index'
-
+  
   #resources
   resources :docs
+  authenticated :user do
+    root "docs#index", as: "authenticated_root"
+  end
+  
+  root 'welcome#index'
 end
